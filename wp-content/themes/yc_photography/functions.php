15ef8c8b542ff45517e0cd8d685dd187edb4d602
Orianne Cielat <orianne.cielat@gmail.com>
2024-01-03 16:27:41 +0100

    setting up functions.php and the assets folder

---
+++ wp-content/themes/yc_photography/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// Must be removed when put into production.
+// Debugging function for development.
+function debug($value) {
+    echo '<div class="custom_debug" style="border: 5px solid grey; max-width: 500px; width: 100%; height: 300px; overflow-y: scroll; overflow-x: hidden; margin: 2em; padding: 1em;background-color: black; color: white; margin: 0 auto;">';
+    echo '<pre style="white-space: break-spaces;">';
+    print_r($value);
+    echo '</pre>';
+    echo '</div>';
+    // die;
+}
